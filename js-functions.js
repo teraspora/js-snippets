6ce9d31853399002c8e38439abf3cab9195f2272
John Lynch <johnlynch.32768@gmail.com>
2019-09-29 23:40:01 +0200

    Add jsonToTable() function

--- js-functions.js
+++ js-functions.js
@@ -16,6 +16,19 @@ function jsonToHTML(jsondata, fields) {
    return datah;
 }
 
+function jsonToTable(jsondata) {
+    let tb_markup = ``;
+    // Get list of fields, based on first entry
+    const fields = Object.keys(jsondata[0]);
+    // Generate markup for table head
+    const th_markup = `<tr>\n${fields.map(field => `    <th>${field}</th>`).join(`\n`)}\n</tr>`;
+    // Generate markup for table body
+    jsondata.forEach(item => {
+        tb_markup += `<tr>\n${fields.map(field => `    <td>${item[field]}</td>`).join(`\n`)}\n</tr>\n`;
+    });
+    return [th_markup, tb_markup];
+}
+
 async function load_json(jsonfile, title, fields, target) {
    const res = await fetch(jsonfile);
    let data = await res.json();
